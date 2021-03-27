28c5e3fe4aeb7a82ecc133ad2af2590291619abf
Diego Koscky <2228939+diegokoscky@users.noreply.github.com>
2021-03-27 11:33:40 -0300

    React icons, react router dom, react scroll, Navbar

--- src/components/Navbar/Index.js
+++ src/components/Navbar/Index.js
@@ -1,14 +1,29 @@
 import React from 'react'
-import { Nav, NavbarContainer, NavLogo } from './NavbarElements'
+import { FaBars } from 'react-icons/fa'
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks
+} from './NavbarElements'
 
 const Navbar = () => {
     return (
         <>
             <Nav>
                 <NavbarContainer>
-                    <NavLogo>
-                        dolla
-                    </NavLogo>
+                    <NavLogo to="/">dolla</NavLogo>
+                    <MobileIcon>
+                        <FaBars/>
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="sobre">Sobre</NavLinks>
+                        </NavItem>
+                    </NavMenu>
                 </NavbarContainer>
             </Nav>
         </>
