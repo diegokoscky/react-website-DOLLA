dc3452a7335ea761e1a0ce7a7cc42249566c2937
Diego Koscky <2228939+diegokoscky@users.noreply.github.com>
2021-04-06 19:17:25 -0300

    Segunda seção do do site

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'sobre',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Banco Premium',
+    headLine: 'Transações ilimitadas com taxa zero',
+    description: 'Tenha acesso ao nosso app exclusivo que te permite fazer transações ilimitadas sem ser cobrado.',
+    buttonLabel: 'Começar',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Carro',
+    dark: true,
+    primary: true,
+    darkText: false
+}
